ff7f86df871d732ca5dd27d772853aee65b0b38d
NyaKyt <71380261+NyaKyt@users.noreply.github.com>
2020-12-22 19:54:22 +0200

    Add files via upload

---
+++ lab8/lab8.2/ButtonPhone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8_task2
+{
+    class ButtonPhone : DiskPhone
+    {
+
+        
+
+        public override void disk()
+        {
+            string chars = " 0 1 2 3 4 5 6 7 8 9 0 * #";
+            Console.WriteLine("Input phone number using {0} :", chars);
+            number = Console.ReadLine();
+        }
+
+    }
+}
---
+++ lab8/lab8.2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("выберить правильный вариант: ");
+            }
+            switch (choice)
+            {
+                case 1:
+                    DiskPhone dp = new DiskPhone();
+                    dp.disk();
+                    break;
+                case 2:
+                    ButtonPhone bp = new ButtonPhone();
+                    bp.disk();
+                    break;
+
+            }
+           /* Console.WriteLine("Button Phone");
+            ButtonPhone bp = new ButtonPhone();
+            //bp.RingTone();
+            Console.WriteLine();
+
+            Console.WriteLine("Black&White Phone");
+            BlackWhitePhone nokia = new BlackWhitePhone();
+            nokia.Call();
+            nokia.RingTone();
+            Console.WriteLine();
+
+            Console.WriteLine("Color Phone");
+            ColorScreenPhone samsung = new ColorScreenPhone();
+            samsung.Call();
+            samsung.SurfTheNet();
+            Console.WriteLine();
+
+            Console.WriteLine("iPhone");
+            iPhone EightPlus = new iPhone();
+            EightPlus.Call();
+            EightPlus.SurfTheNet();
+            Console.WriteLine();
+
+            Console.WriteLine("Google Glass");
+            GoogleGlass coolGlasses = new GoogleGlass();
+            coolGlasses.Call();
+            coolGlasses.RingTone();
+            coolGlasses.SurfTheNet();*/
+
+        }
+    }
+}
